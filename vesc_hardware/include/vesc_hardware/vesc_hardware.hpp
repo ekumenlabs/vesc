676e9c00b160355b6593b49f719e8898011a2382
Gerardo Puga <glpuga@gmail.com>
2026-08-01 17:39:17 -0300

    Adds Id and Iq state interfaces

--- vesc_hardware/include/vesc_hardware/vesc_hardware.hpp
+++ vesc_hardware/include/vesc_hardware/vesc_hardware.hpp
@@ -187,6 +187,8 @@ private:
   // State storage (atomic for thread-safe access from callback)
   std::atomic<double> hw_state_position_;
   std::atomic<double> hw_state_velocity_;
+  std::atomic<double> hw_avg_id_;
+  std::atomic<double> hw_avg_iq_;
 
   // IMU state storage (atomic for thread-safe access from callback)
   std::atomic<double> hw_imu_orientation_x_;
