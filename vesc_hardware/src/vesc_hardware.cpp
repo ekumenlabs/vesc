0adf35260cdbdf0627892f76f648704f29f63e92
Gerardo Puga <glpuga@gmail.com>
2026-08-01 21:44:24 -0300

    Correct creation date

--- vesc_hardware/src/vesc_hardware.cpp
+++ vesc_hardware/src/vesc_hardware.cpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Ekumen, Inc.
+// Copyright 2026 Ekumen, Inc.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
